2b9c138f5a5a04f1812aaa4caa16081709c3f427
wangyf150906 <1542583873@qq.com>
2020-03-15 18:31:03 +0800

    init repo

---
+++ src/test/java/com/wyf/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyf.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
