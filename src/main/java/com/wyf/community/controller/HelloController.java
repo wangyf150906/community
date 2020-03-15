7f6e7905ad5474606ab498ff3ec1272b8db78386
wangyf150906 <1542583873@qq.com>
2020-03-15 19:54:34 +0800

    add index and bootstrap

--- src/main/java/com/wyf/community/controller/HelloController.java
+++ src/main/java/com/wyf/community/controller/HelloController.java
@@ -2,16 +2,16 @@ package com.wyf.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String hello(@RequestParam("name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
 }
